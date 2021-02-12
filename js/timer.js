7621f176eda0bd0d81976c2a997be8bd76595f18
Alex <js.fullstack.development@gmail.com>
2021-02-12 13:12:39 +0200

    hw

--- js/timer.js
+++ js/timer.js
@@ -1,17 +1,15 @@
-// Создай плагин настраиваемого таймера, который ведет обратный отсчет до предварительно определенной даты. Такой плагин может использоваться в блогах и интернет-магазинах, страницах регистрации событий, во время технического обслуживания и т. д.
-
-// Плагин ожидает следующую HTML-разметку и показывает четыре цифры: дни, часы, минуты и секунды в формате XX:XX:XX:XX. Количество дней может состоять из более чем двух цифр.
-
 class CountdownTimer {
   constructor(selector, targetDate) {
-    this.selector = selector;
+    // this.selector = selector;
     this.targetDate = targetDate;
     this.refs = {
-      days: document.querySelector('span[data-value="days"]'),
-      hours: document.querySelector('span[data-value="hours"]'),
-      mins: document.querySelector('span[data-value="mins"]'),
-      secs: document.querySelector('span[data-value="secs"]'),
+      days: document.querySelector(`${selector} [data-value="days"]`),
+      hours: document.querySelector(`${selector} [data-value="hours"]`),
+      mins: document.querySelector(`${selector} [data-value="mins"]`),
+      secs: document.querySelector(`${selector} [data-value="secs"]`),
     };
+
+    this.startTime();
   }
 
   startTime() {
@@ -42,7 +40,5 @@ class CountdownTimer {
   }
 }
 
-const timer1 = new CountdownTimer(
-  '#timer-1',
-  new Date('Jul 01, 2022'),
-).startTime();
+const timer1 = new CountdownTimer('#timer-1', new Date('Jul 01, 2022'));
+const timer2 = new CountdownTimer('#timer-2', new Date('Jun 01, 2023'));
